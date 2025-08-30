c731792cb0bffb588e536d1fe469d2633ae92cad
Melshine <11990946+Melshine@users.noreply.github.com>
2025-08-30 23:59:54 +0200

    Commit first version of the game

---
+++ script.js
@@ -0,0 +1,61 @@
+function getComputerChoice() {
+    const choice = Math.floor(Math.random() * 3);
+    switch (choice) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    choice = prompt('What do you want to play?')
+    return choice.toLowerCase();
+}
+
+
+function playGame() {
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function printWin(humanChoice, computerChoice) {
+        humanScore++
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+    }
+
+    function printLose(humanChoice, computerChoice) {
+        computerScore++
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+    }
+
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice==computerChoice)
+            console.log('Tie!')
+        else if (
+            (humanChoice=="rock" && computerChoice == "scissors") ||
+            (humanChoice=="paper" && computerChoice=="rock") ||
+            (humanChoice=="scissors" && computerChoice=="paper")){
+                printWin(humanChoice, computerChoice)
+            }
+            else {
+                printLose(humanChoice,computerChoice)
+            }
+    }
+
+    for (let i = 0; i < 5; i++) {
+        humanChoice = getHumanChoice()
+        computerChoice = getComputerChoice()
+        playRound(humanChoice, computerChoice)
+    }
+
+    winner = humanScore >= computerScore ? 'YOU' : 'COMPUTER'
+    console.log(`${winner} won the game`)
+}
+
+
+playGame()
+
